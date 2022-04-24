1973b8eeaaf4cb3c5d8e2c16ba02d79d9ce3cd35
stryku <stryku2393@gmail.com>
2022-04-24 23:18:25 +0200

    Made it actually usable

--- include/ct_string/ct_string.hpp
+++ include/ct_string/ct_string.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
 #include <cstddef>
 #include <string>
 #include <string_view>
@@ -16,23 +17,147 @@ namespace detail {
 struct ct_string_base {};
 } // namespace detail
 
+template <typename T>
+constexpr bool is_ct_string = std::is_base_of_v<detail::ct_string_base, T>;
+
+template <typename T>
+concept ct_string_type = is_ct_string<T>;
+
 template <typename Current, typename T>
 concept DifferentTypeString =
     !std::is_same_v<Current, T> && !std::is_base_of_v<T, Current>;
 
 // N does not count null-terminator
+template <typename Char, std::size_t N> struct basic_string_holder {
+
+  using view_t = std::basic_string_view<Char>;
+  using string_t = std::basic_string<Char>;
+  using char_t = Char;
+  static constexpr auto length_v = N;
+
+  constexpr basic_string_holder(view_t str) {
+    std::copy(str.begin(), str.end(), &m_chars[0]);
+    m_chars[N] = Char{'\0'};
+  }
+
+  template <std::size_t U>
+  constexpr auto operator+(const basic_string_holder<char_t, U> &other) const {
+    std::array<char_t, N + U> chars{};
+    const auto self = this->str_view();
+    const auto other_view = other.str_view();
+
+    std::copy(self.begin(), self.end(), chars.data());
+    std::copy(other_view.begin(), other_view.end(),
+              chars.data() + self.length());
+    return basic_string_holder<char_t, N + U>{
+        view_t{chars.data(), chars.size()}};
+  }
+
+  constexpr view_t str_view() const {
+    return view_t{m_chars, N};
+  }
+
+  Char m_chars[N + 1]{};
+};
+
+template <auto StringHolder> struct basic_ct_string2 : detail::ct_string_base {
+
+  using string_holder_t = decltype(StringHolder);
+  using char_t = typename string_holder_t::char_t;
+  using view_t = std::basic_string_view<char_t>;
+  using string_t = std::basic_string<char_t>;
+
+  constexpr bool operator<=>(const basic_ct_string2 &) const = default;
+
+  template <auto Other>
+  constexpr auto operator+(const basic_ct_string2<Other> &) const {
+    constexpr auto combined = StringHolder + Other;
+    return basic_ct_string2<combined>();
+  }
+
+  template <std::size_t U>
+  constexpr bool operator==(const char_t (&other)[U]) const {
+    // Assume string literal. -1 for null-terminator
+    return view_t{other, U - 1} == str_view();
+  }
+
+  template <DifferentTypeString<basic_ct_string2> Other>
+  constexpr bool operator==(const Other &other) const {
+    return view_t{other} == str_view();
+  }
+
+  template <DifferentTypeString<basic_ct_string2> Other>
+  constexpr bool operator!=(const Other &other) const {
+    return !(view_t{other} == str_view());
+  }
+
+  template <std::size_t U>
+  constexpr bool operator!=(const char_t (&other)[U]) const {
+    // Assume string literal. -1 for null-terminator
+    return !(*this == other);
+  }
+
+  template <DifferentTypeString<basic_ct_string2> Other>
+  constexpr bool operator<(const Other &other) const {
+    return view_t{other} < str_view();
+  }
+
+  template <DifferentTypeString<basic_ct_string2> Other>
+  constexpr bool operator<=(const Other &other) const {
+    return view_t{other} <= str_view();
+  }
+
+  template <DifferentTypeString<basic_ct_string2> Other>
+  constexpr bool operator>(const Other &other) const {
+    return !(view_t{other} <= str_view());
+  }
+
+  template <DifferentTypeString<basic_ct_string2> Other>
+  constexpr bool operator>=(const Other &other) const {
+    return !(view_t{other} < str_view());
+  }
+
+  constexpr std::size_t length() const {
+    return str_view().length();
+  }
+
+  constexpr view_t str_view() const {
+    return StringHolder.str_view();
+  }
+
+  constexpr string_t str() const {
+    return string_t{str_view()};
+  }
+
+  constexpr const char_t *c_str() const {
+    return str_view().data();
+  }
+};
+
 template <typename Char, std::size_t N>
 struct basic_ct_string : detail::ct_string_base {
 
   using view_t = std::basic_string_view<Char>;
   using string_t = std::basic_string<Char>;
 
-  consteval basic_ct_string(view_t str) {
+  constexpr basic_ct_string(view_t str) {
     std::copy(str.begin(), str.end(), &m_chars[0]);
     m_chars[N] = Char{'\0'};
   }
 
   constexpr bool operator<=>(const basic_ct_string &) const = default;
+
+  template <std::size_t U>
+  constexpr auto operator+(const basic_ct_string<Char, U> &other) const {
+    std::array<Char, N + U> chars{};
+    const auto self = this->str_view();
+    const auto other_view = other.str_view();
+
+    std::copy(self.begin(), self.end(), chars.data());
+    std::copy(other_view.begin(), other_view.end(),
+              chars.data() + self.length());
+    return basic_ct_string<Char, N + U>{view_t{chars.data(), chars.size()}};
+  }
 
   template <std::size_t U>
   constexpr bool operator==(const Char (&other)[U]) const {
@@ -119,10 +244,10 @@ ct_wstring(const wchar_t (&)[N])
 
 namespace detail {
 
-template <typename Char> struct huge_string_holder {
+template <typename Char> struct oversized_ct_string {
   using value_type = Char;
 
-  constexpr huge_string_holder(const Char *str) {
+  constexpr oversized_ct_string(const Char *str) {
     Char *out = m_str;
     Char *end = m_str + CT_STRING_MAX_STRING_LENGTH - 1; // without the '\0'
 
@@ -135,7 +260,11 @@ template <typename Char> struct huge_string_holder {
     m_length = out - m_str;
   }
 
-  constexpr bool operator==(const huge_string_holder &) const = default;
+  constexpr bool operator==(const oversized_ct_string &) const = default;
+
+  constexpr std::basic_string_view<Char> str_view() const {
+    return std::basic_string_view<Char>{m_str, m_length};
+  }
 
   Char m_str[CT_STRING_MAX_STRING_LENGTH]{};
   std::size_t m_length = 0;
@@ -143,15 +272,25 @@ template <typename Char> struct huge_string_holder {
 
 } // namespace detail
 
+template <typename Char>
+using ct_string_acceptor = detail::oversized_ct_string<Char>;
+
 namespace literals {
-template <detail::huge_string_holder<char> StringHolder>
+template <detail::oversized_ct_string<char> StringHolder>
 consteval auto operator""_cts() {
   return basic_ct_string<char, StringHolder.m_length>{StringHolder.m_str};
 }
 
-template <detail::huge_string_holder<wchar_t> StringHolder>
+template <detail::oversized_ct_string<wchar_t> StringHolder>
 consteval auto operator""_cts() {
   return basic_ct_string<wchar_t, StringHolder.m_length>{StringHolder.m_str};
+}
+
+template <detail::oversized_ct_string<char> OversizedString>
+consteval auto operator""_cts2() {
+  constexpr basic_string_holder<char, OversizedString.m_length> str{
+      OversizedString.m_str};
+  return basic_ct_string2<str>{};
 }
 } // namespace literals
 
