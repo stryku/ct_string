aa42dca7d2b02e0e395101e919ff372e8a527a65
stryku <stryku2393@gmail.com>
2022-04-28 23:00:06 +0200

    Fixed Comparison test

--- include/ct_string/ct_string.hpp
+++ include/ct_string/ct_string.hpp
@@ -41,7 +41,6 @@ template <typename Char, std::size_t N>
 struct basic_string_holder : detail::ct_string_holder_base {
 
   using view_t = std::basic_string_view<Char>;
-  using string_t = std::basic_string<Char>;
   using char_t = Char;
   static constexpr auto length_v = N;
 
@@ -79,19 +78,25 @@ template <typename Char, std::size_t N>
 basic_string_holder(const Char (&)[N]) -> basic_string_holder<Char, N - 1>;
 
 template <detail::ct_string_holder_type auto StringHolder>
-struct basic_ct_string2 : detail::ct_string_base {
+struct basic_ct_string : detail::ct_string_base {
 
   using string_holder_t = decltype(StringHolder);
   using char_t = typename string_holder_t::char_t;
   using view_t = std::basic_string_view<char_t>;
   using string_t = std::basic_string<char_t>;
 
-  constexpr bool operator<=>(const basic_ct_string2 &) const = default;
+  constexpr bool operator<=>(const basic_ct_string &) const = default;
 
   template <auto Other>
-  constexpr auto operator+(const basic_ct_string2<Other> &) const {
+  constexpr auto operator+(const basic_ct_string<Other> &) const {
     constexpr auto combined = StringHolder + Other;
-    return basic_ct_string2<combined>();
+    return basic_ct_string<combined>();
+  }
+
+  template <detail::ct_string_holder_type auto OtherStringHolder>
+  constexpr bool
+  operator==(const basic_ct_string<OtherStringHolder> &other) const {
+    return str_view() == other.str_view();
   }
 
   template <std::size_t U>
@@ -100,12 +105,12 @@ struct basic_ct_string2 : detail::ct_string_base {
     return view_t{other, U - 1} == str_view();
   }
 
-  template <DifferentTypeString<basic_ct_string2> Other>
+  template <DifferentTypeString<basic_ct_string> Other>
   constexpr bool operator==(const Other &other) const {
     return view_t{other} == str_view();
   }
 
-  template <DifferentTypeString<basic_ct_string2> Other>
+  template <DifferentTypeString<basic_ct_string> Other>
   constexpr bool operator!=(const Other &other) const {
     return !(view_t{other} == str_view());
   }
@@ -116,22 +121,22 @@ struct basic_ct_string2 : detail::ct_string_base {
     return !(*this == other);
   }
 
-  template <DifferentTypeString<basic_ct_string2> Other>
+  template <DifferentTypeString<basic_ct_string> Other>
   constexpr bool operator<(const Other &other) const {
     return view_t{other} < str_view();
   }
 
-  template <DifferentTypeString<basic_ct_string2> Other>
+  template <DifferentTypeString<basic_ct_string> Other>
   constexpr bool operator<=(const Other &other) const {
     return view_t{other} <= str_view();
   }
 
-  template <DifferentTypeString<basic_ct_string2> Other>
+  template <DifferentTypeString<basic_ct_string> Other>
   constexpr bool operator>(const Other &other) const {
     return !(view_t{other} <= str_view());
   }
 
-  template <DifferentTypeString<basic_ct_string2> Other>
+  template <DifferentTypeString<basic_ct_string> Other>
   constexpr bool operator>=(const Other &other) const {
     return !(view_t{other} < str_view());
   }
@@ -153,160 +158,163 @@ struct basic_ct_string2 : detail::ct_string_base {
   }
 };
 
-template <typename Char, std::size_t N>
-struct basic_ct_string : detail::ct_string_base {
+// template <typename Char, std::size_t N>
+// struct basic_ct_string : detail::ct_string_base {
 
-  using view_t = std::basic_string_view<Char>;
-  using string_t = std::basic_string<Char>;
+//   using view_t = std::basic_string_view<Char>;
+//   using string_t = std::basic_string<Char>;
 
-  constexpr basic_ct_string(view_t str) {
-    std::copy(str.begin(), str.end(), &m_chars[0]);
-    m_chars[N] = Char{'\0'};
-  }
+//   constexpr basic_ct_string(view_t str) {
+//     std::copy(str.begin(), str.end(), &m_chars[0]);
+//     m_chars[N] = Char{'\0'};
+//   }
 
-  constexpr bool operator<=>(const basic_ct_string &) const = default;
+//   constexpr bool operator<=>(const basic_ct_string &) const = default;
 
-  template <std::size_t U>
-  constexpr auto operator+(const basic_ct_string<Char, U> &other) const {
-    std::array<Char, N + U> chars{};
-    const auto self = this->str_view();
-    const auto other_view = other.str_view();
+//   template <std::size_t U>
+//   constexpr auto operator+(const basic_ct_string<Char, U> &other) const {
+//     std::array<Char, N + U> chars{};
+//     const auto self = this->str_view();
+//     const auto other_view = other.str_view();
 
-    std::copy(self.begin(), self.end(), chars.data());
-    std::copy(other_view.begin(), other_view.end(),
-              chars.data() + self.length());
-    return basic_ct_string<Char, N + U>{view_t{chars.data(), chars.size()}};
-  }
+//     std::copy(self.begin(), self.end(), chars.data());
+//     std::copy(other_view.begin(), other_view.end(),
+//               chars.data() + self.length());
+//     return basic_ct_string<Char, N + U>{view_t{chars.data(), chars.size()}};
+//   }
 
-  template <std::size_t U>
-  constexpr bool operator==(const Char (&other)[U]) const {
-    // Assume string literal. -1 for null-terminator
-    return view_t{other, U - 1} == str_view();
-  }
+//   template <std::size_t U>
+//   constexpr bool operator==(const Char (&other)[U]) const {
+//     // Assume string literal. -1 for null-terminator
+//     return view_t{other, U - 1} == str_view();
+//   }
 
-  template <DifferentTypeString<basic_ct_string> Other>
-  constexpr bool operator==(const Other &other) const {
-    return view_t{other} == str_view();
-  }
+//   template <DifferentTypeString<basic_ct_string> Other>
+//   constexpr bool operator==(const Other &other) const {
+//     return view_t{other} == str_view();
+//   }
 
-  template <DifferentTypeString<basic_ct_string> Other>
-  constexpr bool operator!=(const Other &other) const {
-    return !(view_t{other} == str_view());
-  }
+//   template <DifferentTypeString<basic_ct_string> Other>
+//   constexpr bool operator!=(const Other &other) const {
+//     return !(view_t{other} == str_view());
+//   }
 
-  template <std::size_t U>
-  constexpr bool operator!=(const Char (&other)[U]) const {
-    // Assume string literal. -1 for null-terminator
-    return !(*this == other);
-  }
+//   template <std::size_t U>
+//   constexpr bool operator!=(const Char (&other)[U]) const {
+//     // Assume string literal. -1 for null-terminator
+//     return !(*this == other);
+//   }
 
-  template <DifferentTypeString<basic_ct_string> Other>
-  constexpr bool operator<(const Other &other) const {
-    return view_t{other} < str_view();
-  }
+//   template <DifferentTypeString<basic_ct_string> Other>
+//   constexpr bool operator<(const Other &other) const {
+//     return view_t{other} < str_view();
+//   }
 
-  template <DifferentTypeString<basic_ct_string> Other>
-  constexpr bool operator<=(const Other &other) const {
-    return view_t{other} <= str_view();
-  }
+//   template <DifferentTypeString<basic_ct_string> Other>
+//   constexpr bool operator<=(const Other &other) const {
+//     return view_t{other} <= str_view();
+//   }
 
-  template <DifferentTypeString<basic_ct_string> Other>
-  constexpr bool operator>(const Other &other) const {
-    return !(view_t{other} <= str_view());
-  }
+//   template <DifferentTypeString<basic_ct_string> Other>
+//   constexpr bool operator>(const Other &other) const {
+//     return !(view_t{other} <= str_view());
+//   }
 
-  template <DifferentTypeString<basic_ct_string> Other>
-  constexpr bool operator>=(const Other &other) const {
-    return !(view_t{other} < str_view());
-  }
+//   template <DifferentTypeString<basic_ct_string> Other>
+//   constexpr bool operator>=(const Other &other) const {
+//     return !(view_t{other} < str_view());
+//   }
 
-  constexpr std::size_t length() const {
-    return N;
-  }
+//   constexpr std::size_t length() const {
+//     return N;
+//   }
 
-  constexpr view_t str_view() const {
-    return view_t{m_chars, N};
-  }
+//   constexpr view_t str_view() const {
+//     return view_t{m_chars, N};
+//   }
 
-  constexpr string_t str() const {
-    return string_t{m_chars, N};
-  }
+//   constexpr string_t str() const {
+//     return string_t{m_chars, N};
+//   }
 
-  constexpr const Char *c_str() const {
-    return m_chars;
-  }
+//   constexpr const Char *c_str() const {
+//     return m_chars;
+//   }
 
-  Char m_chars[N + 1]{};
-};
+//   Char m_chars[N + 1]{};
+// };
 
-template <typename Char, std::size_t N>
-basic_ct_string(const Char (&)[N])
-    -> basic_ct_string<Char,
-                       N - 1>; // Assume string literal. -1 for null-terminator
+// template <typename Char, std::size_t N>
+// basic_ct_string(const Char (&)[N])
+//     -> basic_ct_string<Char,
+//                        N - 1>; // Assume string literal. -1 for
+//                        null-terminator
 
-template <std::size_t N> struct ct_string : basic_ct_string<char, N> {
-  consteval ct_string(std::string_view str) : basic_ct_string<char, N>{str} {}
-};
+// template <std::size_t N> struct ct_string : basic_ct_string<char, N> {
+//   consteval ct_string(std::string_view str) : basic_ct_string<char, N>{str}
+//   {}
+// };
 
-template <std::size_t N>
-ct_string(const char (&)[N])
-    -> ct_string<N - 1>; // Assume string literal. -1 for null-terminator
+// template <std::size_t N>
+// ct_string(const char (&)[N])
+//     -> ct_string<N - 1>; // Assume string literal. -1 for null-terminator
 
-template <std::size_t N> struct ct_wstring : basic_ct_string<wchar_t, N> {
-  consteval ct_wstring(std::wstring_view str)
-      : basic_ct_string<wchar_t, N>{str} {}
-};
+// template <std::size_t N> struct ct_wstring : basic_ct_string<wchar_t, N> {
+//   consteval ct_wstring(std::wstring_view str)
+//       : basic_ct_string<wchar_t, N>{str} {}
+// };
 
-template <std::size_t N>
-ct_wstring(const wchar_t (&)[N])
-    -> ct_wstring<N - 1>; // Assume string literal. -1 for null-terminator
+// template <std::size_t N>
+// ct_wstring(const wchar_t (&)[N])
+//     -> ct_wstring<N - 1>; // Assume string literal. -1 for null-terminator
 
-namespace detail {
+// namespace detail {
 
-template <typename Char> struct oversized_ct_string {
-  using value_type = Char;
+// template <typename Char> struct oversized_ct_string {
+//   using value_type = Char;
 
-  constexpr oversized_ct_string(const Char *str) {
-    Char *out = m_str;
-    Char *end = m_str + CT_STRING_MAX_STRING_LENGTH - 1; // without the '\0'
+//   constexpr oversized_ct_string(const Char *str) {
+//     Char *out = m_str;
+//     Char *end = m_str + CT_STRING_MAX_STRING_LENGTH - 1; // without the '\0'
 
-    while (*str && out != end) {
-      *out++ = *str++;
-    }
+//     while (*str && out != end) {
+//       *out++ = *str++;
+//     }
 
-    *out = '\0';
+//     *out = '\0';
 
-    m_length = out - m_str;
-  }
+//     m_length = out - m_str;
+//   }
 
-  constexpr bool operator==(const oversized_ct_string &) const = default;
+//   constexpr bool operator==(const oversized_ct_string &) const = default;
 
-  constexpr std::basic_string_view<Char> str_view() const {
-    return std::basic_string_view<Char>{m_str, m_length};
-  }
+//   constexpr std::basic_string_view<Char> str_view() const {
+//     return std::basic_string_view<Char>{m_str, m_length};
+//   }
 
-  Char m_str[CT_STRING_MAX_STRING_LENGTH]{};
-  std::size_t m_length = 0;
-};
+//   Char m_str[CT_STRING_MAX_STRING_LENGTH]{};
+//   std::size_t m_length = 0;
+// };
 
-} // namespace detail
+// } // namespace detail
 
-template <typename Char>
-using ct_string_acceptor = detail::oversized_ct_string<Char>;
+// template <typename Char>
+// using ct_string_acceptor = detail::oversized_ct_string<Char>;
+
+// namespace literals {
+// template <detail::oversized_ct_string<char> StringHolder>
+// consteval auto operator""_cts2() {
+//   return basic_ct_string<char, StringHolder.m_length>{StringHolder.m_str};
+// }
+
+// template <detail::oversized_ct_string<wchar_t> StringHolder>
+// consteval auto operator""_cts2() {
+//   return basic_ct_string<wchar_t, StringHolder.m_length>{StringHolder.m_str};
+// }
 
 namespace literals {
-template <detail::oversized_ct_string<char> StringHolder>
-consteval auto operator""_cts2() {
-  return basic_ct_string<char, StringHolder.m_length>{StringHolder.m_str};
-}
-
-template <detail::oversized_ct_string<wchar_t> StringHolder>
-consteval auto operator""_cts2() {
-  return basic_ct_string<wchar_t, StringHolder.m_length>{StringHolder.m_str};
-}
-
-template <basic_string_holder OversizedString> consteval auto operator""_cts() {
-  return basic_ct_string2<OversizedString>{};
+template <basic_string_holder StringHolder> consteval auto operator""_cts() {
+  return basic_ct_string<StringHolder>{};
 }
 
 } // namespace literals
--- tests/ct_string_tests.cpp
+++ tests/ct_string_tests.cpp
@@ -4,11 +4,11 @@
 #include <ostream>
 
 namespace ct_string {
-template <typename Char, std::size_t N>
-std::ostream &operator<<(std::ostream &out,
-                         const basic_ct_string<Char, N> &str) {
-  return out << str.str_view();
-}
+// template <typename Char, std::size_t N>
+// std::ostream &operator<<(std::ostream &out,
+//                          const basic_ct_string<Char, N> &str) {
+//   return out << str.str_view();
+// }
 } // namespace ct_string
 
 namespace ct_string::tests {
@@ -17,57 +17,44 @@ using namespace std::literals;
 using namespace ::ct_string::literals;
 
 TEST(CtString, Constructor) {
-  { constexpr ct_string cts{"foo"}; }
-  { constexpr ct_wstring cts{L"foo"}; }
   { constexpr auto cts = "foo"_cts; }
   { constexpr auto cts = L"foo"_cts; }
-  {
-    constexpr const char foo[] = "foo";
-    constexpr ct_string cts{foo};
-  }
 }
 
-// TEST(CtString, Comparison) {
+TEST(CtString, Comparison) {
 
-//   constexpr ct_string foo{"foo"};
-//   constexpr ct_string foo2{"foo"};
-//   constexpr ct_string bar{"bar"};
-//   constexpr ct_string long_baz{"long baz"};
+  constexpr auto foo = "foo"_cts;
+  constexpr auto foo2 = "foo"_cts;
+  constexpr auto bar = "bar"_cts;
+  constexpr auto long_baz = "long baz"_cts;
 
-//   EXPECT_EQ(foo, "foo");
-//   EXPECT_EQ("foo", foo);
-//   EXPECT_EQ(foo, "foo"s);
-//   EXPECT_EQ("foo"s, foo);
-//   EXPECT_EQ(foo, "foo"sv);
-//   EXPECT_EQ("foo"sv, foo);
-//   EXPECT_EQ(foo, foo);
-//   EXPECT_EQ(foo, foo2);
-//   EXPECT_EQ(foo2, foo);
-//   static_assert(foo == foo);
-//   static_assert(foo == foo2);
-//   static_assert(foo2 == foo);
+  static_assert(foo == "foo");
+  static_assert("foo" == foo);
+  static_assert(foo == "foo"sv);
+  static_assert("foo"sv == foo);
+  EXPECT_EQ(foo, "foo"s);
+  EXPECT_EQ("foo"s, foo);
+  static_assert(foo == foo);
+  static_assert(foo == foo2);
+  static_assert(foo2 == foo);
 
-//   EXPECT_NE(foo, "bar");
-//   EXPECT_NE("bar", foo);
-//   EXPECT_NE(foo, "bar"s);
-//   EXPECT_NE("bar"s, foo);
-//   EXPECT_NE(foo, "bar"sv);
-//   EXPECT_NE("bar"sv, foo);
-//   EXPECT_NE(foo, bar);
-//   EXPECT_NE(bar, foo);
-//   static_assert(foo != bar);
-//   static_assert(bar != foo);
+  static_assert(foo != "bar");
+  static_assert("bar" != foo);
+  static_assert(foo != "bar"sv);
+  static_assert("bar"sv != foo);
+  EXPECT_NE(foo, "bar"s);
+  EXPECT_NE("bar"s, foo);
+  static_assert(foo != bar);
+  static_assert(bar != foo);
 
-//   EXPECT_NE(foo, "long baz");
-//   EXPECT_NE("long baz", foo);
-//   EXPECT_NE(foo, "long baz"s);
-//   EXPECT_NE("long baz"s, foo);
-//   EXPECT_NE(foo, "long baz"sv);
-//   EXPECT_NE("long baz"sv, foo);
-//   EXPECT_NE(foo, long_baz);
-//   EXPECT_NE(long_baz, foo);
-//   static_assert(foo != long_baz);
-//   static_assert(long_baz != foo);
-// }
+  static_assert(foo != "long baz");
+  static_assert("long baz" != foo);
+  static_assert(foo != "long baz"sv);
+  static_assert("long baz"sv != foo);
+  EXPECT_NE(foo, "long baz"s);
+  EXPECT_NE("long baz"s, foo);
+  static_assert(foo != long_baz);
+  static_assert(long_baz != foo);
+}
 
 } // namespace ct_string::tests
